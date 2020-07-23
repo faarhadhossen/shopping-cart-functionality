cf377500adb7f5d13f191f9bdcb0d69fd2e258e2
fhrony <fhrony43@gmail.com>
2020-07-23 09:06:09 -0800

    start work with shopping cart functionality project

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('plus-btn').addEventListener('click',()=>{
+    console.log("+11");
+})
+document.getElementById('minus-btn').addEventListener('click',()=>{
+    console.log("-11");
+})
