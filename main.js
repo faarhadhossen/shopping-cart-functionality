9460e781335825c2d5b4e07d416896e224d6a328
fhrony <fhrony43@gmail.com>
2020-07-23 11:10:54 -0800

    products quantity increase and decrease functuinality done

--- main.js
+++ main.js
@@ -1,6 +1,58 @@
-document.getElementById('plus-btn').addEventListener('click',()=>{
-    console.log("+11");
-})
-document.getElementById('minus-btn').addEventListener('click',()=>{
-    console.log("-11");
-})
+//first btn
+
+const plusBtn = document.getElementById('plus-btn').addEventListener('click',()=>{
+    increaseProductPrice('currentQuantity'); 
+});
+const minusBtn = document.getElementById('minus-btn').addEventListener('click',()=>{
+    decreaseProductPrice('currentQuantity'); 
+});
+
+// second btn
+
+const plusBtn2 = document.getElementById('plus-btn2').addEventListener('click',()=>{
+    increaseProductPrice('currentQuantity2'); 
+});
+const minusBtn2 = document.getElementById('minus-btn2').addEventListener('click',()=>{
+    decreaseProductPrice('currentQuantity2'); 
+});
+
+
+
+
+//increase value function
+
+function increaseProductPrice(currentQuantity){
+    let currentValue = document.getElementById(currentQuantity).value;
+    let currentQuantityNum = parseFloat(currentValue);
+    let increaseQuantityNum = currentQuantityNum  + 1;
+    let currentTotalQuantity = document.getElementById(currentQuantity).value =increaseQuantityNum;
+
+    return currentTotalQuantity ;
+}
+
+
+// //decrease value function
+
+function decreaseProductPrice(currentQuantity){
+    let currentValue = document.getElementById(currentQuantity).value;
+
+    if(currentValue <=1){
+        return currentValue;
+    }
+
+    else{
+        let currentQuantityNum = parseFloat(currentValue);
+        let decreaseQuantityNum = currentQuantityNum  - 1;
+        let currentTotalQuantity = document.getElementById(currentQuantity).value =decreaseQuantityNum;
+    
+        return currentTotalQuantity ;
+    }
+
+}
+
+
+
+
+
+
+
